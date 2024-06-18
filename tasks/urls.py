d02f94df8acbcb814389bc10b78e297109a61eab
eli <elijahmurket@gmail.com>
2024-06-18 12:33:52 +0300

    end of logic

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,8 @@
 from . import views
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
-    path('', views.index),
-
+    path('', views.index, name="list"),
+    path('update_task/<str:pk>/', views.updateTask, name='update_task'),    # when you visit the URL http://example.com/update_task/abc123/, Django will capture the string value abc123 from the URL and pass it as the pk parameter to the updateTask view function.
+    path('delete_task/<str:pk>/', views.deleteTask, name='delete'),    
 ]
