af4253ccbee179c0ed1667993dddc3ea4ecc6241
eli <elijahmurket@gmail.com>
2024-06-20 12:06:12 +0300

    added some bootstrap

--- tasks/forms.py
+++ tasks/forms.py
@@ -6,7 +6,8 @@ from .models import *
 
 
 class TaskForm(forms.ModelForm):
-
+    title = forms.CharField(widget= forms.TextInput(attrs={'placeholder':'Add new task.....'}))
     class Meta:         
         model = Task       #the model we are creating a form for
         fields = '__all__'       #specifiying the fields from the model we want to show in the form
+        
